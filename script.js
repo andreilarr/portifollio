4c247c3d89e2a1e51f06591796c3a974f239cde8
Andrei Larroque <andrei.larroque@hotmail.com>
2022-02-27 15:28:05 -0300

    commit final

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ $(document).ready(function(){
 			$('.navbar').addClass("sticky");
 		}else{
 			$('.navbar').removeClass("sticky");
+		}if(this.scrollY > 500){
+			$('.scroll-up-btn').addClass("show");
+		}else{
+			$('.scroll-up-btn').removeClass("show");
 		}
 	});
+	$('.scroll-up-btn').click(function(){
+		$('html').animate({scrollTop: 0});
+	});
+
+	var typed = new Typed(".typing", {
+		strings:["Desenvolvedor","Programador"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop:true
+	})
+
+
+	var typed = new Typed(".typing-2", {
+		strings:["Desenvolvedor","Programador"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop:true
+	})
+
+
 	//toggle menu/navbar script
 	$('.menu-btn').click(function(){
 		$('.navbar .menu').toggleClass("active");
